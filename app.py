9bba25c57580e873f7b658abfcb7217d22ac18fa
Zoha-R <zoharaza4@gmail.com>
2023-09-30 15:41:43 +0500

    flask frontend backend created

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello python with flask"
+    name = input("user tell us your name")
+    return render_template("home.html",data = {'name':name} )
+
+@app.route("/about" ,methods=['GET','POST'])
+def about():
+    if request.method=="POST":
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        result= num1 + num2 
+
+        return render_template("about.html",result = {"result":result})
+
+    else:
+        return render_template('about.html',result = {"result":"none"})
+
 
 if __name__ == "__main__":
 
